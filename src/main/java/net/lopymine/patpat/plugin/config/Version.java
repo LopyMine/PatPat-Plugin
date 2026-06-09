0705c6b6b31be4fa2c9c773436665578b6904384
feeland <gugber37@gmail.com>
2026-06-10 01:19:51 +0300

    micro fix

--- src/main/java/net/lopymine/patpat/plugin/config/Version.java
+++ src/main/java/net/lopymine/patpat/plugin/config/Version.java
@@ -66,6 +66,7 @@ public record Version(int major, int minor, int patch) implements Comparable<Ver
 				this.patch == version.patch;
 	}
 
+	@NotNull
 	@Override
 	public String toString() {
 		return "%d.%d.%d".formatted(this.major, this.minor, this.patch);
